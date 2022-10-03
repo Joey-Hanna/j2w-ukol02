47b2c668501c4b5db4d1f8092f4ea068dd4ab7c7
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-10-03 22:33:15 +0200

    uprava mezer v Controlleru + pridani 'private final' k int rozmerSeznamuCitaty

--- src/main/java/cz/czechitas/java2webapps/ukol2/CitatyController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/CitatyController.java
@@ -1,19 +1,12 @@
 package cz.czechitas.java2webapps.ukol2;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
 import java.util.Random;
 
-
-
-
-
 @Controller
 public class CitatyController {
-
     private static final List<String> citaty = List.of(
             "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
             "A user interface is like a joke. If you have to explain it, it's not that good.",
@@ -24,9 +17,7 @@ public class CitatyController {
             "Programmer (noun.): A machine that turns coffee into code.",
             "Real programmers count from 0.");
 
-
-    int rozmerSeznamuCitaty = citaty.size();
-
+    private final int rozmerSeznamuCitaty = citaty.size();
     private final Random random = new Random();
 
     @GetMapping("/")
@@ -37,7 +28,6 @@ public class CitatyController {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("citat", nahodnyCitat);
         modelAndView.addObject("obrazek", cisloNahodnehoObrazku);
-
         return modelAndView;
     }
 
